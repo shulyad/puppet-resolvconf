e5c294ae4d2f3498c9571ab961ddfab3ab2b8c37
Danil S. <dvshulgin@yandex.ru>
2015-06-08 16:03:50 +0300

    Add 2 exec ot reload.pp
    1. add rm /bin/rm /run/resolvconf/interface/*
    2. /bin/sed -i \'/# DNS/!s/dns-.*$/# DNS managed by Puppet!  &/\' /etc/network/interfaces

--- manifests/reload.pp
+++ manifests/reload.pp
@@ -6,4 +6,11 @@ class resolvconf::reload {
   exec { '/sbin/resolvconf -u':
     refreshonly => true,
   }
+  exec { '/bin/rm /run/resolvconf/interface/*':
+    refreshonly => true,
+    onlyif      => ["/usr/bin/test -f /run/resolvconf/interface/eth*"]
+  }
+  exec { '/bin/sed -i \'/# DNS/!s/dns-.*$/# DNS managed by Puppet!  &/\' /etc/network/interfaces':
+    refreshonly => true,
+  }
 }
